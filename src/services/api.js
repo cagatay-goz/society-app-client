4b5b8d776baa77fa7ac27a2c655b0d6d2a58bbe8
eraykabalak <eraykabalak19@gmail.com>
2024-12-25 00:42:06 +0200

    Create Api.js for Improved Code Design
    - Introduced Api.js to centralize API calls, enhancing code structure and maintainability.
    - Simplified and streamlined API interaction across the application.
    
    Enhance AnnouncementForm for User Submissions
    
    - Updated AnnouncementForm to allow users to post announcements seamlessly.
    - Integrated backend handling for processing form submissions, including file uploads and data storage.
    - Streamlined user experience for creating announcements with necessary details and optional images.

---
+++ src/services/api.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+// Fetch societies
+export const fetchSocieties = async () => {
+    try {
+        const response = await axios.get("/societies"); // Proxy kullanıldığı için baseURL gerekmez
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching societies:", error.message);
+        throw error;
+    }
+};
+
+// Fetch announcements for a specific society by societyId
+export const fetchAnnouncementsBySocietyId = async (societyId) => {
+    try {
+        const response = await axios.get(`/announcements/society/${societyId}`); // Backend'deki doğru endpoint'i kullan
+        return response.data; // Duyuruları döndür
+    } catch (error) {
+        console.error(`Error fetching announcements for society ID ${societyId}:`, error.message);
+        throw error;
+    }
+};
+
+// Fetch a single announcement by announcementId
+export const fetchAnnouncementById = async (announcementId) => {
+    try {
+        const response = await axios.get(`/announcements/${announcementId}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching announcement ID ${announcementId}:`, error.message);
+        throw error;
+    }
+};
+
+
+/**
+ * Create a new announcement
+ */
+export const createAnnouncement = async (formData) => {
+    try {
+        const response = await axios.post(`/announcements`, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error creating announcement:", error);
+        throw error;
+    }
+};
